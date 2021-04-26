0ae608cc156130766a2e8104bff177cbbd0db3a5
armanhowk <armanhowk@gmail.com>
2021-04-26 14:18:25 +0200

    refactoring to multi mudule maven project. closes #1

---
+++ pet-clinic-web/src/test/java/guru/springframework/sfgpetclinicTest/SpringPetClinicApplicationTests.java
@@ -0,0 +1,19 @@
+package guru.springframework.sfgpetclinicTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+
+
+@SpringBootApplication
+@ContextConfiguration
+@SpringBootTest
+class SpringPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
